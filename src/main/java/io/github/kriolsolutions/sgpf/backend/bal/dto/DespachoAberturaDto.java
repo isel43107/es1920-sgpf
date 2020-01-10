0371f2bc25b25644319530ffaf5ddbf7560b15f2
Paulo Borges <poolborges@hotmail.com>
2020-01-10 18:41:24 +0000

    upgrade DTO

---
+++ src/main/java/io/github/kriolsolutions/sgpf/backend/bal/dto/DespachoAberturaDto.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2020 kriolSolutions.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.kriolsolutions.sgpf.backend.bal.dto;
+
+import io.github.kriolsolutions.sgpf.backend.dal.entidades.docs.Despacho;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author pauloborges
+ */
+@Data
+@NoArgsConstructor
+public class DespachoAberturaDto {
+    
+    private Long candidaturaId;
+    private Long gestorFinanciamentoId;
+    private Despacho.DespachoDecisao decisao;
+}
