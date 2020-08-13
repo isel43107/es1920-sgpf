f65b25e93dc296d6051353585a725226dd5d3bd8
Paulo Borges <poolborges@hotmail.com>
2020-08-13 14:37:52 +0100

    update About View

--- src/main/java/io/github/kriolsolutions/sgpf/web/ui/about/AboutView.java
+++ src/main/java/io/github/kriolsolutions/sgpf/web/ui/about/AboutView.java
@@ -4,15 +4,14 @@ import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.html.Span;
-import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import io.github.kriolsolutions.sgpf.web.ui.MainLayout;
 
 @Route(value = "About", layout = MainLayout.class)
 @PageTitle("About")
-public class AboutView extends HorizontalLayout {
+public class AboutView extends VerticalLayout {
 
     public static final String VIEW_NAME = "About";
 
@@ -21,7 +20,7 @@ public class AboutView extends HorizontalLayout {
         //setJustifyContentMode(JustifyContentMode.START);
         //setAlignItems(Alignment.START);
         
-        add(new H1(VaadinIcon.INFO_CIRCLE.create(), new Span( "Informação sobre SGPF 2019 " )) );
+        add(new H1(new Span( "Sobre SGPF 2019" )) );
         add(new H3( "Autores" ));
         add(new Paragraph( "Paulo Borges" ));
         add(new Paragraph( "Rafael Delgado" ));
